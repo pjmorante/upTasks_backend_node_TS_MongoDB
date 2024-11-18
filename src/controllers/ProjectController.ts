eec720361b7feef6af08e9829eb6c9ec63e0613c
Pedro Morante <pedro.morante@outlook.com>
2024-11-18 15:14:00 -0500

    feat: add create project controller

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,6 +1,18 @@
 import type {Request, Response} from 'express';
+import Project from '../models/Project';
 
 export class ProjectController {
+
+  static createProject = async (req: Request, res: Response) => {
+    const project = new Project(req.body);    
+    try {
+      await project.save();
+      //await Project.create(req.body);
+      res.send('Proyecto creado correctamente!')
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   static getAllProjects = async (req: Request, res: Response) => {
     res.send('Todos los proyectos');

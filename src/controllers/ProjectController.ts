7999bcca6ff0dc90e08ccd0e238e9460fa565489
Pedro Morante <pedro.morante@outlook.com>
2024-11-20 13:22:28 -0500

    feat: add updateProject and deleteProject endpoints

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -23,15 +23,47 @@ export class ProjectController {
   };
 
   static getProjectById = async (req: Request, res: Response) => {
-    const {id} = req.params
+    const { id } = req.params;
     try {
-      const project = await Project.findById(id)
+      const project = await Project.findById(id);
       if (!project) {
         const error = new Error('Proyecto No Encontrado');
-        res.status(404).json({error: error.message});
-        return
+        res.status(404).json({ error: error.message });
+        return;
       }
       res.json(project);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static updateProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findByIdAndUpdate(id, req.body);
+      if (!project) {
+        const error = new Error('Proyecto No Encontrado');
+        res.status(404).json({ error: error.message });
+        return;
+      }
+      await project.save();
+      res.send('Proyecto actualizado.');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static deleteProject = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const project = await Project.findById(id);
+      await project.deleteOne();
+      if (!project) {
+        const error = new Error('Proyecto No Encontrado');
+        res.status(404).json({ error: error.message });
+        return;
+      }
+      res.send('Proyecto eliminado.');
     } catch (error) {
       console.log(error);
     }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -21,4 +21,27 @@ router.get('/:id',
   ProjectController.getProjectById
 );
 
+router.put(
+  '/:id',
+  param('id').isMongoId().withMessage('Id no válido!'),
+  body('projectName')
+    .notEmpty()
+    .withMessage('El nombre del proyecto es Obligatorio'),
+  body('clientName')
+    .notEmpty()
+    .withMessage('El nombre del cliente es Obligatorio'),
+  body('description')
+    .notEmpty()
+    .withMessage('La descripción del proyecto es Obligatorio'),
+  handleInputErrors,
+  ProjectController.updateProject
+);
+
+router.delete(
+  '/:id',
+  param('id').isMongoId().withMessage('Id no válido!'),
+  handleInputErrors,
+  ProjectController.deleteProject
+);
+
 export default router;
